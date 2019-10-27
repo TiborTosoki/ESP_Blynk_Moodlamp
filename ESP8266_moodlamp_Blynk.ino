8903ced6afee8d604f642c4a54518a6aa670017f
TiborTosoki <tibor.tosoki@gmail.com>
2019-10-27 23:18:43 +0100

    started Blynk implementation

--- ESP8266_moodlamp_Blynk.ino
+++ ESP8266_moodlamp_Blynk.ino
@@ -1,48 +1,38 @@
 #include <FastLED.h>
+#include <BlynkSimpleEsp8266.h>
+#include <ESP8266WiFi.h>
 
-const int LED_PIN = 5;
-const int NUM_LEDS = 10;
-
+#define LED_PIN 5
+#define NUM_LEDS 10
 CRGB leds[NUM_LEDS];
+
+int R, G, B;
+
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-
+  //Serial.begin(9600);
+  Blynk.begin("Feyfg_NmoUQRsrC8N-hs4te_2kMCcSbu", "Tosoki", NULL);
 }
 
+
+BLYNK_WRITE(V0) {
+  R = param[0].asInt();
+  G = param[1].asInt();
+  B = param[2].asInt();
+  constantColor();
+}
+
+
 void loop() {
-     for (int led = 0; led < NUM_LEDS; led++) {
-        leds[led] = CRGB(0, 0, 0);
-        FastLED.show();
-        delay(200);
-      }
-
-    for (int led = 0; led < NUM_LEDS; led++) {
-        leds[led] = CRGB(120, 255, 0);
-        FastLED.show();
-        delay(200);
-      }
-    
-  
-  delay(3000);
-
-  //  for (int val = 255; val > 0; val -= 4) {
-  //    for (int led = 0; led < NUM_LEDS; led++) {
-  //      leds[led] = CHSV(120, 255, val);
-  //      FastLED.show();
-  //    }
-  //  }
-  //  delay(300);
+  Blynk.run();
+}
 
 
-    leds[2].fadeLightBy(255);
-    leds[3].fadeLightBy(255);
-    FastLED.show();
-    delay(2000);
-    leds[2].setRGB(120,255,0);
-    leds[2].fadeLightBy(230);
-    leds[3].setRGB(120,255,0);
-    leds[3].fadeLightBy(230);
-    FastLED.show();
-    delay(2000);
+
+void constantColor(){
+  for (int led = 0; led < NUM_LEDS; led++) {
+    leds[led] = CRGB(R, G, B);
+  }
+  FastLED.show();
 }
